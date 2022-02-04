a3b9bed31c589e1fb92198779f2c5f401ed88277
Drew Krause <drewkrause2@gmail.com>
2022-02-03 22:50:50 -0600

    Massive "UI" redo, I don't know how half of it works

--- sync.js
+++ sync.js
@@ -1,51 +1,68 @@
 var gameShadowRoot = document.getElementsByTagName("game-app")[0].shadowRoot;
 var server = "www.drewkrause.dev";
 
-var div = document.createElement("div");
-div.classList.add("setting");
+var userDiv = document.createElement("div");
+userDiv.classList.add("setting");
 
+
+// =============================
+// I DO NOT UNDERSTAND THIS CODE
+// =============================
+
+// Create title and description
 var settingText = document.createElement("div");
 settingText.className = "text";
+settingText.innerHTML = "<div class=\"title\">User</div><div class=\"description\">Password to sync devices with server.</div>";
+
+userDiv.appendChild(settingText);
+
+// Create div for inputs
 var settingButtons = document.createElement("div");
 settingButtons.className = "control";
 
-var title = document.createElement("div");
-title.className = "title";
-title.innerHTML = "Sync";
+// Add div for inputs to main div
+userDiv.appendChild(settingButtons);
 
-var desc = document.createElement("div");
-desc.className = "description";
-desc.innerHTML = "Synchronize stats with a 3rd party server";
+// Create new catagory for sync buttons
+var syncButtons = userDiv.cloneNode(true)
 
+// Create textbox for user
 var user = document.createElement("input");
 user.type = "password"
+user.style.verticalAlign = "middle";
+settingButtons.appendChild(user);
 
+// Create upload button
 var upButton = document.createElement("button");
+// Theme upload button
 upButton.style.border = "none";
 upButton.style.background = "none";
 upButton.style.padding = "0 4px"
 upButton.style.verticalAlign = "middle";
+// Add icon to button
 upButton.innerHTML = "<svg width=\"32px\" height=\"32px\" viewBox=\"0 0 24 24\" fill=\"none\" xmlns=\"http://www.w3.org/2000/svg\"><path d=\"M11 4a4 4 0 0 0-3.999 4.102 1 1 0 0 1-.75.992A3.002 3.002 0 0 0 7 15h1a1 1 0 1 1 0 2H7a5 5 0 0 1-1.97-9.596 6 6 0 0 1 11.169-2.4A6 6 0 0 1 16 17a1 1 0 1 1 0-2 4 4 0 1 0-.328-7.987 1 1 0 0 1-.999-.6A4.001 4.001 0 0 0 11 4zm.293 5.293a1 1 0 0 1 1.414 0l2 2a1 1 0 0 1-1.414 1.414L13 12.414V20a1 1 0 1 1-2 0v-7.586l-.293.293a1 1 0 0 1-1.414-1.414l2-2z\" fill=\"var(--color-tone-3)\"/></svg>";
 
 // Ctrl + C, Ctrl + V
 var downButton = upButton.cloneNode();
 downButton.innerHTML = "<svg width=\"32px\" height=\"32px\" viewBox=\"0 0 24 24\" fill=\"none\" xmlns=\"http://www.w3.org/2000/svg\"><path d=\"M11 4a4 4 0 0 0-3.999 4.102 1 1 0 0 1-.75.992A3.002 3.002 0 0 0 7 15h1a1 1 0 1 1 0 2H7a5 5 0 0 1-1.97-9.596 6 6 0 0 1 11.169-2.4A6 6 0 0 1 16 17a1 1 0 1 1 0-2 4 4 0 1 0-.328-7.987 1 1 0 0 1-.999-.6A4.001 4.001 0 0 0 11 4zm1 6a1 1 0 0 1 1 1v7.586l.293-.293a1 1 0 0 1 1.414 1.414l-2 2a1 1 0 0 1-1.414 0l-2-2a1 1 0 1 1 1.414-1.414l.293.293V11a1 1 0 0 1 1-1z\" fill=\"var(--color-tone-3)\"/></svg>";
 
-settingButtons.appendChild(user);
-settingButtons.appendChild(upButton);
-settingButtons.appendChild(downButton);
-
-settingText.appendChild(title);
-settingText.appendChild(desc);
-div.appendChild(settingText);
-div.appendChild(settingButtons);
+// Add upload and download buttons to 2nd div
+syncButtons.children[1].appendChild(upButton);
+syncButtons.children[1].appendChild(downButton);
+// Set title and description of second div
+syncButtons.children[0].innerHTML = "<div class=\"title\">Sync</div><div class=\"description\">Sync your devices with the server.</div>";
 
 var game = gameShadowRoot.getElementById("game");
+
+// ========================
+// Begin understanding code 
+// ========================
 
 gameShadowRoot.getElementById("settings-button").addEventListener("click", function() {
     // Structure: game-settings > shadowRoot > div.sections > 1st div.section 
     var settings = game.getElementsByTagName("game-settings")[0].shadowRoot.children[1].children[0];
-    settings.appendChild(div);
+    settings.appendChild(userDiv);
+    settings.appendChild(syncButtons);
 });
 
 upButton.addEventListener("click", function() {
@@ -70,7 +87,7 @@ upButton.addEventListener("click", function() {
 
 downButton.addEventListener("click", function() {
     if (!user.value) return;
-    var cleanedUser = user.value.replace(/\W/g, '');
+    var cleanedUser = user.value.replace(/\W/g, ''); // Remove all non-alphanumeric characters (stolen from stackoverflow [how did copilot know i was going to type this?])
     var params = JSON.stringify({
         "user": cleanedUser
     });
