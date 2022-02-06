6fa3298c2ecf67f8173573e497466ae01fd2f9c7
Drew Krause <drewkrause2@gmail.com>
2022-02-05 18:40:00 -0600

    Adds toasts to make results more apparent

--- sync.js
+++ sync.js
@@ -40,6 +40,7 @@ upButton.style.border = "none";
 upButton.style.background = "none";
 upButton.style.padding = "0 4px"
 upButton.style.verticalAlign = "middle";
+upButton.style.cursor = "pointer";
 // Add icon to button
 upButton.innerHTML = "<svg width=\"32px\" height=\"32px\" viewBox=\"0 0 24 24\" fill=\"none\" xmlns=\"http://www.w3.org/2000/svg\"><path d=\"M11 4a4 4 0 0 0-3.999 4.102 1 1 0 0 1-.75.992A3.002 3.002 0 0 0 7 15h1a1 1 0 1 1 0 2H7a5 5 0 0 1-1.97-9.596 6 6 0 0 1 11.169-2.4A6 6 0 0 1 16 17a1 1 0 1 1 0-2 4 4 0 1 0-.328-7.987 1 1 0 0 1-.999-.6A4.001 4.001 0 0 0 11 4zm.293 5.293a1 1 0 0 1 1.414 0l2 2a1 1 0 0 1-1.414 1.414L13 12.414V20a1 1 0 1 1-2 0v-7.586l-.293.293a1 1 0 0 1-1.414-1.414l2-2z\" fill=\"var(--color-tone-3)\"/></svg>";
 
@@ -83,6 +84,10 @@ upButton.addEventListener("click", function() {
     xhr.open("POST", url, true);
     xhr.setRequestHeader("Content-type", "application/json");
 
+    xhr.onload = () => {
+        wordleToast(xhr.status == 200 ? "Successfully uploaded data." : "Failed to upload data.", 0, true);
+    }
+
     xhr.send(params);
     console.log(JSON.parse(params));
 });
@@ -99,14 +104,12 @@ downButton.addEventListener("click", function() {
     var xhr = new XMLHttpRequest();
 
     xhr.onload = () => {
+        if (xhr.status != 200) return (wordleToast("Failed to download data.", 0, true));
         var response = JSON.parse(xhr.responseText);
-        
-        
-        // log response
         console.log(response);
         if (response.stats) localStorage.setItem("statistics", JSON.stringify(response.stats));
         if (response.game) localStorage.setItem("gameState", JSON.stringify(response.game));
-
+        wordleToast("Successfully downloaded data. Refreshing.", 0, true);
         window.location.reload();
     };
 
@@ -115,3 +118,11 @@ downButton.addEventListener("click", function() {
 
     xhr.send(params);
 });
+
+// Rewrite of the toast function in Wordle
+function wordleToast(text, duration, system) {
+    var toast = document.createElement("game-toast");
+    toast.setAttribute("text", text);
+    if (duration) toast.setAttribute("duration", duration);
+    gameShadowRoot.querySelector(system ? "#system-toaster" : "#game-toaster").prepend(toast);
+}
